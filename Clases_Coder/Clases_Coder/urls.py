ff8d023c307a62be01148f77c441a81e0b4ca70b
Cesar <cesarbaby@gmail.com>
2023-05-26 18:10:12 -0300

    Borrado de views.py y editado del urls.py que estaban en la carpeta de config

--- Clases_Coder/Clases_Coder/urls.py
+++ Clases_Coder/Clases_Coder/urls.py
@@ -15,20 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import (
-    saludo,
-    muestra_nombre,
-    probando_template,
-    usando_loader,
-    curso
-)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludo/', saludo),
-    path('muestra_nombre/<nombre>/', muestra_nombre),
-    path('probando_template/', probando_template),
-    path('usando_loader/', usando_loader),
-    path('curso/<nombre>/<numero>/', curso),
+    path('AppCoder', include('AppCoder.urls'))
 ]
