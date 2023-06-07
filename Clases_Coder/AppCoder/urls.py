197a875811ed896336974c1422f293eaf92db440
Cesar <cesarbaby@gmail.com>
2023-06-07 20:57:23 -0300

    Agregamos formularios y búsquedas en DB

--- Clases_Coder/AppCoder/urls.py
+++ Clases_Coder/AppCoder/urls.py
@@ -22,5 +22,8 @@ urlpatterns = [
     path('profesores/', views.profesores, name="Profesores"),
     path('estudiantes/', views.estudiantes, name="Estudiantes"),
     path('cursos/', views.cursos, name="Cursos"),
-    path('entregables/', views.entregables, name="Entregables")
+    path('entregables/', views.entregables, name="Entregables"),
+    path('form-comun/', views.form_comun, name="Form-Comun"),
+    path('form-con-api/', views.form_con_api, name="Form-Con-Api"),
+    path('buscar-form-con-api/', views.buscar_form_con_api, name="Buscar-Form-Con-Api")
 ]
--- Clases_Coder/AppCoder/views.py
+++ Clases_Coder/AppCoder/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Curso
+from AppCoder.forms import CursoFormulario, BuscaCursoForm
 
 def inicio(request):
     return render(request, "AppCoder/index.html")
@@ -14,3 +16,43 @@ def estudiantes(request):
 
 def entregables(request):
     return render(request, "AppCoder/entregables.html")
+
+def form_comun(request):
+
+    if request.method == 'POST':
+
+        curso =  Curso(nombre=request.POST['curso'],camada=request.POST['camada'])
+        curso.save()
+
+        return render(request, "AppCoder/index.html")
+
+    return render(request,"AppCoder/form_comun.html")
+
+def form_con_api(request):
+    if request.method == "POST":
+        miFormulario = CursoFormulario(request.POST) # Aqui me llega la informacion del html
+        # print(miFormulario)
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            curso = Curso(nombre=informacion["curso"], camada=informacion["camada"])
+            curso.save()
+            return render(request, "AppCoder/index.html")
+    else:
+        miFormulario = CursoFormulario()
+
+    return render(request, "AppCoder/form_con_api.html", {"miFormulario": miFormulario})
+
+def buscar_form_con_api(request):
+    if request.method == "POST":
+        miFormulario = BuscaCursoForm(request.POST) # Aqui me llega la informacion del html
+
+        if miFormulario.is_valid():
+            informacion = miFormulario.cleaned_data
+            
+            cursos = Curso.objects.filter(nombre__icontains=informacion["curso"])
+
+            return render(request, "AppCoder/resultados_buscar_form.html", {"cursos": cursos})
+    else:
+        miFormulario = BuscaCursoForm()
+
+    return render(request, "AppCoder/buscar_form_con_api.html", {"miFormulario": miFormulario})
