df4285aceeefea26976288c420fd198d138cbefa
Cesar <cesarbaby@gmail.com>
2023-10-09 20:00:20 -0300

    Se agrega TestCase de la placa aunque no funciona correctamente

--- Clases_Coder/users/tests.py
+++ Clases_Coder/users/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+import random
+import string
+from AppCoder.models import Profesor
 
 from datetime import datetime as dt
 from .utilities.utility import return_today
@@ -9,3 +12,23 @@ class TestUtilities(TestCase):
     def test_day(self):
         hoy = dt.now()
         self.assertEqual(hoy.day, return_today())
+
+class TestUsers(TestCase):
+
+    def test_crear_profesor(self):
+        informacion = {
+            "apellido": "",
+            "email": "coder@coder.com"
+        }
+        KEY_LEN = 20
+        keylistNombre = [random.choice((string.ascii_letters + string.digits)) for i in range(KEY_LEN)]
+        nombrePrueba = "".join(keylistNombre)
+
+        print(f"----> Prueba con: {nombrePrueba}")
+
+        profesor = Profesor(
+            nombre=nombrePrueba,
+            apellido=informacion["apellido"],
+            email=informacion["email"],
+        )
+        profesor.save()
