fad8479ed7e835993b39bdde18d7db8f9ae6fe2f
Cesar <cesarbaby@gmail.com>
2024-07-21 01:24:27 -0300

    Reparado bugs

--- Clases_Coder/AppCoder/urls.py
+++ Clases_Coder/AppCoder/urls.py
@@ -29,7 +29,6 @@ clase_21 = [
     path('form-comun/', views.curso_formulario, name="Form-Comun"),
     path('form-con-api/', views.form_con_api, name="Form-Con-Api"),
     path('buscar-form-con-api/', views.buscar_form_con_api, name="Buscar_Form_Con_Api"),
-    path('mostrar-cursos/', views.mostrar_cursos, name="Mostrar_Cursos"),
 ]
 
 urlpatterns += clase_21
